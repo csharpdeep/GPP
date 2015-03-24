78e9df4c1ed655c14ad85c1d77a114be3b032e05
vutiendung <vudung94nb@gmail.com>
2015-03-24 10:28:23 +0700

    --Dung form dang nhap va file ConnectSQL

---
+++ GPP/Common/ConnectSQL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace GPP.Common
+{
+    class ConnectSQL
+    {
+        private static ConnectSQL _connectSQL = new ConnectSQL();
+        private static SqlConnection connect;
+        private SqlDataAdapter da;
+        private DataTable dt;
+        private string SQLString;
+        private ConnectSQL()
+        {
+            if (System.IO.Directory.Exists(Application.StartupPath + "Data/config.xml")==false)
+            {
+                SettingSQL s = new SettingSQL();
+                s.ShowDialog();
+            }
+            ReadXML();
+            connect = new SqlConnection(SQLString);
+            connect.Open();
+        }
+        public void ReadXML()
+        {
+            try
+            {
+                string tenfile = @"Data/Config.xml";
+                XmlDocument doc = new XmlDocument();
+                doc.Load(tenfile);
+                //Lấy dữ liệu từ file đổ vào XmlNodeList
+                XmlNode goc = doc.DocumentElement;
+                XmlNodeList ds = doc.SelectNodes("/application");
+                foreach (XmlNode node in ds)
+                {
+                    XmlNode serverName = node.FirstChild;
+                    XmlNode userNameDatabase = serverName.NextSibling;
+                    XmlNode passWordsDatabase = userNameDatabase.NextSibling;
+                    XmlNode databaseName = passWordsDatabase.NextSibling;
+
+
+                    string _serverName = serverName.InnerText;
+                    string _databaseName = databaseName.InnerText;
+                    string _userNameDatabase = userNameDatabase.InnerText;
+                    string _passWordsDatabase = passWordsDatabase.InnerText;
+                    SQLString="Data Source="+_serverName+","+"Initial Catalog="+_databaseName;
+
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        public static ConnectSQL Istance
+        {
+            get
+            {
+                return _connectSQL;
+            }
+        }
+        public DataTable Getdata(string sql)
+        {
+            da = new SqlDataAdapter(sql, connect);
+            dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public bool ThucThiSQL(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, connect);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ GPP/View/FormLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GPP.Common;
+
+namespace GPP.View
+{
+    public partial class FormLogin : Form
+    {
+        private string _userName, _passWord;
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPassWord.Text != "" && txtUser.Text != "")
+            {
+    
+                _userName = txtUser.Text;
+                _passWord = txtPassWord.Text;
+                DataTable userTable = new DataTable();
+                string qr = "select TaiKhoan,MatKhau from user where TaiKhoan='" + _userName + "'";
+                userTable = ConnectSQL.Istance.Getdata(qr);
+                if (userTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("Ten dang nhap hoac tai khoan khong ton tai!");
+                }
+                {
+                    if (_passWord == userTable.Rows[0][1].ToString())
+                    {
+                        MessageBox.Show("Dang nhap thanh cong!");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập hoặc mật khẩu không đúng.\nVui lòng kiểm tra và đăng nhập lại","Lỗi",MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+            }
+        }
+    }
+}
