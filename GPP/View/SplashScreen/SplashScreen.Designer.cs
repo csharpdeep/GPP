68a7024ae95270bd2b62af84b3a2f31bcdcb7b7d
TrinhXuanDong <info.dongtx@gmail.com>
2015-03-27 02:25:27 +0700

    Create splash screen

---
+++ GPP/View/SplashScreen/SplashScreen.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace GPP
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this._progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _progressBar
+            // 
+            this._progressBar.Location = new System.Drawing.Point(12, 305);
+            this._progressBar.Name = "_progressBar";
+            this._progressBar.Size = new System.Drawing.Size(575, 23);
+            this._progressBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(533, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ver 1.0";
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.BackgroundImage = global::GPP.Properties.Resources.Background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(599, 340);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._progressBar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar _progressBar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ GPP/View/SplashScreen/SplashScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPP
+{
+    public partial class SplashScreen : Form
+    {
+        private Timer _timer;
+        public SplashScreen()
+        {
+            InitializeComponent();
+            _timer = new Timer();
+            _timer.Interval = 50;
+            _timer.Tick += OnTimerTick;
+            _timer.Enabled = true;
+            _timer.Start();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            _progressBar.Value++;
+            if (_progressBar.Value == _progressBar.Maximum)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                MainWindow frmMain = new MainWindow();
+                this.ShowInTaskbar = false;
+                this.Hide();
+                frmMain.ShowDialog();
+            }
+        }
+    }
+}
