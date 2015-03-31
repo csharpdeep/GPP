88c70a0e486791e27c5240af665092629c421a51
vutiendung <vudung94nb@gmail.com>
2015-03-31 15:56:10 +0700

    Form Don Vi Tinh by Dung

---
+++ GPP/View/DonViTinh/frmDonViTinhUC.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace GPP.View.DonViTinh
+{
+    partial class frmDonViTinhUC
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _dataGridView
+            // 
+            this._dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this._dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._dataGridView.Location = new System.Drawing.Point(0, 0);
+            this._dataGridView.Name = "_dataGridView";
+            this._dataGridView.Size = new System.Drawing.Size(769, 471);
+            this._dataGridView.TabIndex = 0;
+            this._dataGridView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnDataGridviewCellEndEdit);
+            this._dataGridView.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.OnUserAddedRow);
+            this._dataGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.OnUserDeletingRow);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaDonVi";
+            this.Column1.HeaderText = "Mã đơn vị tính";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DataPropertyName = "MoTa";
+            this.Column2.HeaderText = "Mô tả đơn vị tính";
+            this.Column2.Name = "Column2";
+            // 
+            // frmDonViTinhUC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._dataGridView);
+            this.Name = "frmDonViTinhUC";
+            this.Size = new System.Drawing.Size(769, 471);
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView _dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
---
+++ GPP/View/DonViTinh/frmDonViTinhUC.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using DongTX.Core;
+
+namespace GPP.View.DonViTinh
+{
+    public partial class frmDonViTinhUC : UserControl
+    {
+        public frmDonViTinhUC()
+        {
+            InitializeComponent();
+            _dataGridView.DataSource = SqlHelper.Instance.ExecuteDataTable("SELECT * FROM DONVITINH");
+        }
+
+        private void OnDataGridviewCellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            //khi 1 row nào đó sửa dữ liệu xong thì row đó
+            // có thể là row mới hoặc row cũ cần update
+            int index = e.RowIndex;
+            string maDonVi = _dataGridView[0, index].Value.ToString();
+            string moTa = _dataGridView[1, index].Value.ToString();
+
+            bool isUpdate = SqlHelper.Instance.CheckExistKey("DONVITINH", "MaDonVi", maDonVi);
+            if (isUpdate)
+            {
+                int recordEffect = SqlHelper.Instance.Update("DONVITINH", new SqlParameter[]
+                {
+                    new SqlParameter("MoTa", moTa)
+                },
+                new SqlParameter[]
+                {
+                    new SqlParameter("MaDonVi", maDonVi)
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể sửa dữ liệu này", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                //them moi du lieu
+                int recordEffect = (int)SqlHelper.Instance.Insert("DONVITINH", new SqlParameter[]
+                {
+                    new SqlParameter("MaDonVi",maDonVi),
+                    new SqlParameter("MoTa",moTa),
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể thêm mới dữ liệu", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void OnUserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            //khi user thêm 1 dòng mới thì ta sẽ lấy mã kế tiếp
+            int rowIndex = e.Row.Index - 1;
+            _dataGridView[0, rowIndex].Value = SqlHelper.Instance.GetNextPrimaryKey("DONVITINH", "MaDonVi", "DV00000000001");
+        }
+
+        private void OnUserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            int index = e.Row.Index;
+            string maChucVu = _dataGridView[0, index].Value.ToString();
+            DialogResult dlg = MessageBox.Show(
+                "Bạn thực sự muốn xóa "
+                + maChucVu
+                + "? \nĐiều này sẽ làm cho các dữ liệu liên quan đến mã này có thể bị xóa theo \nHãy xem xét kỹ lưỡng trước khi xóa!",
+                "Cảnh báo",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2);
+            if (dlg == DialogResult.Yes)
+            {
+                //trước khi xóa dữ liệu này, ta sẽ xóa dữ liệu trong bảng ChucNang 
+                //tương ứng với mã chức vụ chuẩn bị được xóa
+                //xoa du lieu trong database
+                int recordEffect = SqlHelper.Instance.Delete("DONVITINH", new SqlParameter[]
+                {
+                    new SqlParameter("MaDonVi",maChucVu)
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể xóa dữ liệu này!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
