8ea12cbf3618f4758aff7252ce8363327c8536fb
vutiendung <vudung94nb@gmail.com>
2015-03-31 11:20:32 +0700

    Form Loai thuoc

---
+++ GPP/View/LoaiThuoc/frmLoaiThuoc.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace GPP
+{
+    partial class frmLoaiThuoc
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._dataGridView = new System.Windows.Forms.DataGridView();
+            this.MaLoaiThuoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenLoaiThuoc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _dataGridView
+            // 
+            this._dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this._dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaLoaiThuoc,
+            this.TenLoaiThuoc});
+            this._dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._dataGridView.Location = new System.Drawing.Point(0, 0);
+            this._dataGridView.Name = "_dataGridView";
+            this._dataGridView.Size = new System.Drawing.Size(922, 524);
+            this._dataGridView.TabIndex = 0;
+            this._dataGridView.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.OnDataGridviewEndClick);
+            this._dataGridView.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.OnDataGridviewAddRow);
+            this._dataGridView.UserDeletingRow += new System.Windows.Forms.DataGridViewRowCancelEventHandler(this.OnUserDeletingRow);
+            // 
+            // MaLoaiThuoc
+            // 
+            this.MaLoaiThuoc.DataPropertyName = "MaLoaiThuoc";
+            this.MaLoaiThuoc.Frozen = true;
+            this.MaLoaiThuoc.HeaderText = "Ma Loai Thuoc";
+            this.MaLoaiThuoc.Name = "MaLoaiThuoc";
+            this.MaLoaiThuoc.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.MaLoaiThuoc.Width = 150;
+            // 
+            // TenLoaiThuoc
+            // 
+            this.TenLoaiThuoc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenLoaiThuoc.DataPropertyName = "TenLoaiThuoc";
+            this.TenLoaiThuoc.HeaderText = "Ten Loai Thuoc";
+            this.TenLoaiThuoc.Name = "TenLoaiThuoc";
+            // 
+            // frmLoaiThuoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this._dataGridView);
+            this.Name = "frmLoaiThuoc";
+            this.Size = new System.Drawing.Size(922, 524);
+            ((System.ComponentModel.ISupportInitialize)(this._dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView _dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaLoaiThuoc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenLoaiThuoc;
+    }
+}
---
+++ GPP/View/LoaiThuoc/frmLoaiThuoc.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DongTX.Core;
+using System.Data.SqlClient;
+
+namespace GPP
+{
+    public partial class frmLoaiThuoc : UserControl
+    {
+        public frmLoaiThuoc()
+        {
+            InitializeComponent();
+            _dataGridView.DataSource = SqlHelper.Instance.ExecuteDataTable("SELECT * FROM LOAITHUOC");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void OnDataGridviewEndClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //khi 1 row nào đó sửa dữ liệu xong thì row đó
+            // có thể là row mới hoặc row cũ cần update
+            int index = e.RowIndex;
+            string maLoaiThuoc = _dataGridView[0, index].Value.ToString();
+            string tenLoaiThuoc = _dataGridView[1, index].Value.ToString();
+
+            bool isUpdate = SqlHelper.Instance.CheckExistKey("LOAITHUOC", "MaLoaiThuoc", maLoaiThuoc);
+            if (isUpdate)
+            {
+                int recordEffect = SqlHelper.Instance.Update("LOAITHUOC", new SqlParameter[]
+                {
+                    new SqlParameter("MaLoaiThuoc", maLoaiThuoc)
+                },
+                new SqlParameter[]
+                {
+                    new SqlParameter("TenLoaiThuoc", tenLoaiThuoc)
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể sửa dữ liệu này", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                //them moi du lieu
+                int recordEffect = (int)SqlHelper.Instance.Insert("LOAITHUOC", new SqlParameter[]
+                {
+                    new SqlParameter("MaLoaiThuoc",maLoaiThuoc),
+                    new SqlParameter("TenLoaiTHuoc",tenLoaiThuoc),
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể thêm mới dữ liệu", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void OnDataGridviewAddRow(object sender, DataGridViewRowEventArgs e)
+        {
+            //khi user thêm 1 dòng mới thì ta sẽ lấy mã kế tiếp
+            int rowIndex = e.Row.Index - 1;
+            _dataGridView[0, rowIndex].Value = SqlHelper.Instance.GetNextPrimaryKey("LoaiTHuoc", "MaLoaiThuoc", "LT000000000000000001");
+        }
+
+        private void OnUserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+            int index = e.Row.Index;
+            string maLoaiThuoc = _dataGridView[0, index].Value.ToString();
+            DialogResult dlg = MessageBox.Show(
+                "Bạn thực sự muốn xóa "
+                + maLoaiThuoc
+                + "? \nĐiều này sẽ làm cho các dữ liệu liên quan đến mã này có thể bị xóa theo \nHãy xem xét kỹ lưỡng trước khi xóa!",
+                "Cảnh báo",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2);
+            if (dlg == DialogResult.Yes)
+            {
+                //xoa du lieu trong database
+                int recordEffect = SqlHelper.Instance.Delete("LoaiThuoc", new SqlParameter[]
+                {
+                    new SqlParameter("MaLoaiThuoc",maLoaiThuoc)
+                });
+
+                if (recordEffect <= 0)
+                {
+                    MessageBox.Show("Không thể xóa dữ liệu này!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
